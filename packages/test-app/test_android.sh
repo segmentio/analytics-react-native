74c469f2458efd56c2453fe8faae28fee68d2de4
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-10-13 19:30:07 +0200

    chore(test-app): improve android tester

--- packages/test-app/test_android.sh
+++ packages/test-app/test_android.sh
@@ -3,15 +3,11 @@
 set -e
 
 pushd project
-    # rm -rf node_modules
-    # yarn
-
     pushd android
         export GRADLE_OPTS="-Xmx1024m"
 
-        # Upgrade the build tools to 3.1.4 and Gradle to 4.4
-        ./gradlew wrapper --gradle-version=4.4
-        sed -i.bak s/tools.build:gradle:2.2.3/tools.build:gradle:3.1.4/g build.gradle
+        # Upgrade the Gradle wrapper to 4.6
+        ./gradlew wrapper --gradle-version=4.6
 
         yarn react-native link
     popd
