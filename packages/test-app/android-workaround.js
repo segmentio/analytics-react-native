87b4c6ff699190ecfdaedc1dd49ce95ccddd3216
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2018-10-23 21:53:53 +0200

    style(prettier): fix format

--- packages/test-app/android-workaround.js
+++ packages/test-app/android-workaround.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-
 // As of today (Oct 23) react-native is broken on Android
 // We're fixing this by patching the repository order in the main gradle file of the Android project
 // See:
@@ -13,9 +12,9 @@ const path = require('path')
 const gradlePath = path.resolve(__dirname, 'project/android/build.gradle')
 
 fs.writeFileSync(
-    gradlePath,
-    fs
-        .readFileSync(gradlePath, 'utf-8')
-        .replace(/google\(\)/g, '')
-        .replace(/jcenter\(\)/g, 'google()\njcenter()')
+	gradlePath,
+	fs
+		.readFileSync(gradlePath, 'utf-8')
+		.replace(/google\(\)/g, '')
+		.replace(/jcenter\(\)/g, 'google()\njcenter()')
 )
