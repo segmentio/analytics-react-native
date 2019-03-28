b978af580181601c143c689d22ccc3715e21f693
Fathy Boundjadj <fathy.boundjadj@gmail.com>
2019-03-28 19:42:41 +0100

    chore(test-app): 5 seconds interval

--- packages/test-app/e2e/main.spec.js
+++ packages/test-app/e2e/main.spec.js
@@ -40,7 +40,7 @@ const callProperties = {
 
 async function hasMatchingCall(type, name, props) {
 	const properties = callProperties[type]
-	const interval = 1000 // 1 second
+	const interval = 10 * 1000 // 10 seconds
 	const timeout = 5 * 60 * 1000 // 5 minutes
 	const start = Date.now()
 
