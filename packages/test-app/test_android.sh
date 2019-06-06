7144474f8139149c00fc7d17b97a650313340d58
Kadi Kraman <kadi.kraman@formidable.com>
2019-06-06 14:37:22 +0100

    chore(test-app): add detox command to package.json

--- packages/test-app/test_android.sh
+++ packages/test-app/test_android.sh
@@ -6,7 +6,7 @@ pushd project
     yarn react-native link
 popd
 
-yarn detox build --configuration android
+yarn detox:build --configuration android
 
 # Android E2E tests are not working yet
-# yarn detox test --configuration android
+# yarn detox:test --configuration android
