5a31c1e1eec2d68818ae9a0a8125cdb1064e4baa
Alan Charles <alan.charles@segment.com>
2021-11-16 13:37:40 -0700

    refactor: remove babel plugin change jest config

--- packages/plugins/plugin-facebook-app-events/jest.config.js
+++ packages/plugins/plugin-facebook-app-events/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'react-native',
-  setupFiles: ['./src/__tests__/__helpers__/setup.js'],
+  setupFiles: ['../../core/src/__tests__/__helpers__/setup.js'],
   testPathIgnorePatterns: ['<rootDir>/src/__tests__/__helpers__/'],
   modulePathIgnorePatterns: ['/lib/'],
 };
+
