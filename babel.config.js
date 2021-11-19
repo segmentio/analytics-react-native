709e84f3bcb8293a327f91fdd44f93623e573369
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2021-11-19 15:47:54 -0800

    fix: removing unused babel mapping

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,3 @@
-const path = require('path');
-
-const pak = require('./packages/core/package.json');
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          [pak.name]: path.join(__dirname, 'packages', 'core', pak.source),
-        },
-      },
-    ],
-  ],
 };
