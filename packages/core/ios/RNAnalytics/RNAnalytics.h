702ffb42d27adaf5c755494145cf807eb8c863f0
Massimo Necchi <massimo.necchi@rvu.co.uk>
2022-02-04 15:52:12 +0000

    fix: iOS import for expo 44 compatibility

--- packages/core/ios/RNAnalytics/RNAnalytics.h
+++ packages/core/ios/RNAnalytics/RNAnalytics.h
@@ -6,11 +6,7 @@
 //  Copyright © 2020 Segment.io, Inc. All rights reserved.
 //
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridgeModule.h>
-#endif
 
 @interface RNAnalytics: NSObject<RCTBridgeModule>
 
