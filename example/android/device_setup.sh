ad73c6d925170aae992877ab0eabcbc5b3343ca6
Alan Charles <acharles@twilio.com>
2022-06-16 15:00:19 -0600

    refactor: bump ci sdk to 31

--- example/android/device_setup.sh
+++ example/android/device_setup.sh
@@ -6,18 +6,18 @@ emulator=$ANDROID_HOME/emulator/emulator
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-if ! $avdmanager list avd -c | grep -q 'Pixel_API_21_AOSP';
+if ! $avdmanager list avd -c | grep -q 'Pixel_API_31_AOSP';
 then
   echo "${green}=> Installing emulator ${reset}"
   $sdkmanager --install emulator
   
-  echo "${green}=> Adding SDK 21 x86_64${reset}"
-  $sdkmanager "system-images;android-21;default;x86_64"
+  echo "${green}=> Adding SDK 31 arm64-v8a${reset}"
+  $sdkmanager "system-images;android-31;default;arm64-v8a"
   
-  echo "${green}=> Creating Pixel_API_21_AOSP emulator${reset}"
-  $avdmanager create avd -n Pixel_API_21_AOSP -d pixel_xl --package "system-images;android-21;default;x86_64" 
+  echo "${green}=> Creating Pixel_API_31_AOSP emulator${reset}"
+  $avdmanager create avd -n Pixel_API_31_AOSP -d pixel_xl --package "system-images;android-31;default;arm64-v8a" 
 fi
 
-$emulator -avd Pixel_API_21_AOSP -no-snapshot -noaudio -no-boot-anim &
+$emulator -avd Pixel_API_31_AOSP -no-snapshot -noaudio -no-boot-anim &
 
-echo "${green}📱  Pixel_API_21_AOSP emulator ready!${reset}"
+echo "${green}📱  Pixel_API_31_AOSP emulator ready!${reset}"
