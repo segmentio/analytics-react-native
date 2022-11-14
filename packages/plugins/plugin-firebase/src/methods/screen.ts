169fec1e3c2674f8fc410765c1cf4c8be5602414
Alan Charles <acharles@twilio.com>
2022-11-14 09:38:27 -0700

    fix: add ability to send custom props in screen events firebase

--- packages/plugins/plugin-firebase/src/methods/screen.ts
+++ packages/plugins/plugin-firebase/src/methods/screen.ts
@@ -2,8 +2,11 @@ import firebaseAnalytics from '@react-native-firebase/analytics';
 import type { ScreenEventType } from '@segment/analytics-react-native';
 
 export default async (event: ScreenEventType) => {
-  await firebaseAnalytics().logScreenView({
+  const screenProps = {
     screen_name: event.name,
     screen_class: event.name,
-  });
+    ...event.properties,
+  };
+
+  await firebaseAnalytics().logScreenView(screenProps);
 };
