8752ffa7d0e1fc1c39db0ad5326bd633ed5e819b
Oscar Bazaldua <511911+oscb@users.noreply.github.com>
2022-11-14 11:48:45 -0800

    ci: fix detox tests, upgrade RN/Detox deps in e2e example

---
+++ example/e2e/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  maxWorkers: 1,
+  testTimeout: 240000,
+  rootDir: '..',
+  testMatch: ['<rootDir>/e2e/**/*.e2e.js'],
+  verbose: true,
+  reporters: ['detox/runners/jest/reporter'],
+  globalSetup: 'detox/runners/jest/globalSetup',
+  globalTeardown: 'detox/runners/jest/globalTeardown',
+  testEnvironment: 'detox/runners/jest/testEnvironment',
+};
