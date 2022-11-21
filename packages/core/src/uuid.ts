346f57556de7cb5b885a7fb4d489ba766b97421d
Alan Charles <acharles@twilio.com>
2022-11-21 14:43:35 -0700

    fix: add return value to uuid in dev mode

--- packages/core/src/uuid.ts
+++ packages/core/src/uuid.ts
@@ -1,5 +1,11 @@
 import { getNativeModule } from './util';
 
 export const getUUID = (): string => {
+  //returns a string in DEV to avoid native modules
+  //and to work with chrome debugging tools
+  if (__DEV__) {
+    return 'DEV-UUID';
+  }
+
   return getNativeModule('AnalyticsReactNative')?.getUUIDSync() ?? '';
 };
