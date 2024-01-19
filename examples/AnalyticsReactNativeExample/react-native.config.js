87f34e3ae318f85666ef1b8b94b5646e138bda17
Neek Sandhu <neek.sandhu@outlook.com>
2024-01-19 11:55:13 -0800

    fix: RN 0.72.7 -> 0.72.9 (#909)

--- examples/AnalyticsReactNativeExample/react-native.config.js
+++ examples/AnalyticsReactNativeExample/react-native.config.js
@@ -1,11 +1,6 @@
 const { rootMap } = require('./workspace') // Load the linked data
 
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true,
-    }
-  },
   dependencies: {
     // Local packages need to be referenced here to be linked to the app
     // This is not required in a standalone production app
