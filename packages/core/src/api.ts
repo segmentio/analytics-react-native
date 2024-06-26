be84847e400ef77f4502c478fc77e9369205da10
Harsh Vardhan <harsh.vardhan@segment.com>
2024-06-26 16:08:14 +0530

    limit batch size

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -9,13 +9,19 @@ export const uploadEvents = async ({
   url: string;
   events: SegmentEvent[];
 }) => {
+   const context = events.find((event) => !!event.context)?.context;
+  const integrations = events.find((event) => !!event.integrations)?.integrations;
+  let sentEvents = events.map(({ context, integrations, ...event }) => event);
+  const requestBody = JSON.stringify({
+    batch: sentEvents,
+    sentAt: new Date().toISOString(),
+    writeKey: writeKey,
+   context: context,
+    integrations: integrations
+  });
   return await fetch(url, {
     method: 'POST',
-    body: JSON.stringify({
-      batch: events,
-      sentAt: new Date().toISOString(),
-      writeKey: writeKey,
-    }),
+    body: requestBody,
     headers: {
       'Content-Type': 'application/json; charset=utf-8',
     },
