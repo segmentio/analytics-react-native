99d9955aff8dd8529784b6762fba760eaae63f99
Sunita Prajapati <sprajapati@twilio.com>
2025-06-02 16:11:43 +0530

    fix: bug reproduce

--- examples/AnalyticsReactNativeExample/App.tsx
+++ examples/AnalyticsReactNativeExample/App.tsx
@@ -1,186 +1,352 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-// import RNBootSplash from 'react-native-bootsplash';
-import {
-  NavigationContainer,
-  NavigationState,
-  PartialState,
-} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {
-  createClient,
-  AnalyticsProvider,
-  CountFlushPolicy,
-  ConsentPlugin,
-  // @ts-ignore unused for e2e tests
-  // StartupFlushPolicy,
-  // @ts-ignore unused for e2e tests
-  // TimerFlushPolicy,
-} from '@segment/analytics-react-native';
+// import 'react-native-gesture-handler';
+// import * as React from 'react';
+// // import RNBootSplash from 'react-native-bootsplash';
+// import {
+//   NavigationContainer,
+//   NavigationState,
+//   PartialState,
+// } from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
+// import {
+//   createClient,
+//   AnalyticsProvider,
+//   CountFlushPolicy,
+//   ConsentPlugin,
+//   SegmentClient,
+//   // @ts-ignore unused for e2e tests
+//   // StartupFlushPolicy,
+//   // @ts-ignore unused for e2e tests
+//   // TimerFlushPolicy,
+// } from '@segment/analytics-react-native';
+// import Home from './Home';
+// import SecondPage from './SecondPage';
+// import Modal from './Modal';
+// import {useEffect, useState} from 'react';
+// //import {Logger} from './plugins/Logger';
+
+// // import {AmplitudeSessionPlugin} from '@segment/analytics-react-native-plugin-amplitude-session';
+// // import { ConsentManager } from './plugins/ConsentManager';
+// // import { FirebasePlugin } from '@segment/analytics-react-native-plugin-firebase';
+// // import { FacebookAppEventsPlugin } from '@segment/analytics-react-native-plugin-facebook-app-events';
+// // import { IdfaPlugin } from '@segment/analytics-react-native-plugin-idfa';
+// // import { AdvertisingIdPlugin } from '@segment/analytics-react-native-plugin-advertising-id';
+// // import { ClevertapPlugin } from '@segment/analytics-react-native-plugin-clevertap';
+// // import { BrazePlugin } from '@segment/analytics-react-native-plugin-braze';
+// import OTPublishersNativeSDK from 'react-native-onetrust-cmp';
+// import { OneTrustConsentProvider } from './consent'
+
+// // OTPublishersNativeSDK.startSDK(
+// //   'cdn.cookielaw.org',
+// //   '018f7e67-ea32-753c-9220-2eb2c94f452b-test',
+// //   'en',
+// //   {countryCode: 'us', regionCode:'ca'},
+// //   true,
+// // ).then((responseObject) => {
+// //   const typedResponse = responseObject as {responseString:string};
+// //   console.info('Download status is ' + typedResponse.responseString);
+// //   // get full JSON object from responseObject.responseString
+// // })
+// // .catch((error) => {
+// //   console.error(`OneTrust download failed with error ${error}`);
+// // });
+
+
+
+// // const segmentClient = createClient({
+// //   writeKey: 'VRzdzieruD0trB00Tl7n8cdQDyEI8qIK',
+// //   trackAppLifecycleEvents: true,
+// //   collectDeviceId: true,
+// //   debug: true,
+// //   trackDeepLinks: true,
+// //   flushPolicies: [
+// //     new CountFlushPolicy(5),
+// //     // These are disabled for E2E tests
+// //     // new TimerFlushPolicy(1000),
+// //     // new StartupFlushPolicy(),
+// //   ],
+// // });
+// // console.log('check for consent settings in segment client', segmentClient.consentSettings.get());
+// // const applicableCategories = ['C0004'];
+
+// // const oneTrustProvider = new OneTrustConsentProvider(OTPublishersNativeSDK)
+// // oneTrustProvider.setApplicableCategories(applicableCategories);
+// // const oneTrustPlugin = new ConsentPlugin(oneTrustProvider);
+
+// // segmentClient.add({ plugin: oneTrustPlugin });
+
+// // oneTrustPlugin.start()
+// // // const LoggerPlugin = new Logger();
+
+// // // segmentClient.add({plugin: LoggerPlugin});
+
+// // // To see an example Consent Manager uncomment the following
+// // // const ConsentManagerPlugin = new ConsentManager();
+// // // segmentClient.add({ plugin: ConsentManagerPlugin });
+
+// // // To test the Firebase plugin make sure to add your own API_KEY in example/ios/GoogleService-Info.plist
+// // // segmentClient.add({ plugin: new FirebasePlugin() });
+
+// // // To test the Facebook App Events plugin make sure to add your Facebook App Id to Info.plist
+// // // segmentClient.add({ plugin: new FacebookAppEventsPlugin() });
+// // // const idfaPlugin = new IdfaPlugin();
+// // // segmentClient.add({ plugin: idfaPlugin });
+
+// // // segmentClient.add({plugin: new AmplitudeSessionPlugin()});
+
+// // // segmentClient.add({ plugin: new BrazePlugin() });
+
+// // // segmentClient.add({ plugin: new ClevertapPlugin() });
+
+// // // segmentClient.add({
+// // //   plugin: new AdvertisingIdPlugin(),
+// // // });
+
+// const MainStack = createStackNavigator();
+// const RootStack = createStackNavigator();
+// function MainStackScreen() {
+//   return (
+//     <MainStack.Navigator
+//       screenOptions={{
+//         headerStyle: {
+//           backgroundColor: '#262e4f',
+//         },
+//         headerTintColor: '#fff',
+//         headerTitleStyle: {
+//           fontWeight: 'bold',
+//         },
+//       }}>
+//       <MainStack.Screen
+//         name="Home"
+//         component={Home}
+//         options={{headerShown: false}}
+//       />
+//       <MainStack.Screen
+//         name="SecondPage"
+//         component={SecondPage}
+//         options={{title: 'Second Page'}}
+//       />
+//     </MainStack.Navigator>
+//   );
+// }
+
+// const getActiveRouteName = (
+//   state: NavigationState | PartialState<NavigationState> | undefined,
+// ): string => {
+//   if (!state || typeof state.index !== 'number') {
+//     return 'Unknown';
+//   }
+
+//   const route = state.routes[state.index];
+
+//   if (route.state) {
+//     return getActiveRouteName(route.state);
+//   }
+
+//   return route.name;
+// };
+
+// const App = () => {
+//   // React.useEffect(() => {
+//   //   void RNBootSplash.hide();
+//   // }, []);
+
+//   const [segmentClient, setSegmentClient] = useState<SegmentClient | undefined>(undefined);
+//   const [routeName, setRouteName] = useState('Unknown');
+//   useEffect(() => {
+//     const initializeSDKs = async () => {
+//       try {
+//         const responseObject = await OTPublishersNativeSDK.startSDK(
+//           'cdn.cookielaw.org',
+//           '018f7e67-ea32-753c-9220-2eb2c94f452b-test',
+//           'en',
+//           { countryCode: 'us', regionCode: 'ca' },
+//           true,
+//         );
+
+//         const typedResponse = responseObject as { responseString: string };
+//         console.info('Download status is ' + typedResponse.responseString);
+
+//         // Proceed with Segment initialization
+//         const client = createClient({
+//           writeKey: 'VRzdzieruD0trB00Tl7n8cdQDyEI8qIK',
+//           trackAppLifecycleEvents: true,
+//           collectDeviceId: true,
+//           debug: true,
+//           trackDeepLinks: true,
+//           flushPolicies: [new CountFlushPolicy(5)],
+//         });
+
+//         const applicableCategories = ['C0004'];
+//         const oneTrustProvider = new OneTrustConsentProvider(OTPublishersNativeSDK);
+//         oneTrustProvider.setApplicableCategories(applicableCategories);
+
+//         const oneTrustPlugin = new ConsentPlugin(oneTrustProvider);
+//         client.add({ plugin: oneTrustPlugin });
+
+//         oneTrustPlugin.start();
+
+//         setSegmentClient(client);
+//       } catch (error) {
+//         console.error(`OneTrust download failed with error ${error}`);
+//       }
+//     };
+
+//     initializeSDKs();
+//   }, []);
+
+//   return (
+//     <AnalyticsProvider client={segmentClient}>
+//       <NavigationContainer
+//         onStateChange={state => {
+//           const newRouteName = getActiveRouteName(state);
+
+//           if (segmentClient && routeName !== newRouteName) {
+//             void segmentClient.screen(newRouteName);
+
+//             setRouteName(newRouteName);
+//           }
+//         }}>
+//         <RootStack.Navigator
+//           screenOptions={{
+//             headerStyle: {
+//               backgroundColor: '#262e4f',
+//             },
+//             headerTintColor: '#fff',
+//             headerTitleStyle: {
+//               fontWeight: 'bold',
+//             },
+//             presentation: 'modal',
+//           }}>
+//           <RootStack.Screen
+//             name="Main"
+//             component={MainStackScreen}
+//             options={{headerShown: false}}
+//           />
+//           <RootStack.Screen
+//             name="Modal"
+//             component={Modal}
+//             options={{headerBackTitle: 'Go back'}}
+//           />
+//         </RootStack.Navigator>
+//       </NavigationContainer>
+//     </AnalyticsProvider>
+//   );
+// };
+
+// export default App;
+
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AnalyticsProvider } from '@segment/analytics-react-native';
+import { createClient, ConsentPlugin, CountFlushPolicy } from '@segment/analytics-react-native';
+import type { SegmentClient } from '@segment/analytics-react-native';
+
+import OTPublishersNativeSDK from 'react-native-onetrust-cmp';
+import { OneTrustConsentProvider } from './consent';
+
 import Home from './Home';
 import SecondPage from './SecondPage';
 import Modal from './Modal';
-import {useState} from 'react';
-//import {Logger} from './plugins/Logger';
-
-// import {AmplitudeSessionPlugin} from '@segment/analytics-react-native-plugin-amplitude-session';
-// import { ConsentManager } from './plugins/ConsentManager';
-// import { FirebasePlugin } from '@segment/analytics-react-native-plugin-firebase';
-// import { FacebookAppEventsPlugin } from '@segment/analytics-react-native-plugin-facebook-app-events';
-// import { IdfaPlugin } from '@segment/analytics-react-native-plugin-idfa';
-// import { AdvertisingIdPlugin } from '@segment/analytics-react-native-plugin-advertising-id';
-// import { ClevertapPlugin } from '@segment/analytics-react-native-plugin-clevertap';
-// import { BrazePlugin } from '@segment/analytics-react-native-plugin-braze';
-import OTPublishersNativeSDK from 'react-native-onetrust-cmp';
-import { OneTrustConsentProvider } from '@segment/analytics-react-native-plugin-onetrust'
-
-OTPublishersNativeSDK.startSDK(
-  'storageLocation',
-  'domainIdentifier',
-  'en',
-  {countryCode: 'us', regionCode:'ca'},
-  true,
-).then((responseObject) => {
-  console.info('Download status is ' + responseObject);
-  // get full JSON object from responseObject.responseString
-})
-.catch((error) => {
-  console.error(`OneTrust download failed with error ${error}`);
-});
-
-
-
-const segmentClient = createClient({
-  writeKey: 'KDzygiYHVtaOeNDmSTzcGDH2KMkJCPTM',
-  trackAppLifecycleEvents: true,
-  collectDeviceId: true,
-  debug: true,
-  trackDeepLinks: true,
-  flushPolicies: [
-    new CountFlushPolicy(5),
-    // These are disabled for E2E tests
-    // new TimerFlushPolicy(1000),
-    // new StartupFlushPolicy(),
-  ],
-});
-
-const oneTrustProvider = new OneTrustConsentProvider(OTPublishersNativeSDK)
-const oneTrustPlugin = new ConsentPlugin(oneTrustProvider);
-
-segmentClient.add({ plugin: oneTrustPlugin });
-
-oneTrustPlugin.start()
-
-// const LoggerPlugin = new Logger();
-
-// segmentClient.add({plugin: LoggerPlugin});
-
-// To see an example Consent Manager uncomment the following
-// const ConsentManagerPlugin = new ConsentManager();
-// segmentClient.add({ plugin: ConsentManagerPlugin });
-
-// To test the Firebase plugin make sure to add your own API_KEY in example/ios/GoogleService-Info.plist
-// segmentClient.add({ plugin: new FirebasePlugin() });
-
-// To test the Facebook App Events plugin make sure to add your Facebook App Id to Info.plist
-// segmentClient.add({ plugin: new FacebookAppEventsPlugin() });
-// const idfaPlugin = new IdfaPlugin();
-// segmentClient.add({ plugin: idfaPlugin });
-
-// segmentClient.add({plugin: new AmplitudeSessionPlugin()});
-
-// segmentClient.add({ plugin: new BrazePlugin() });
-
-// segmentClient.add({ plugin: new ClevertapPlugin() });
-
-// segmentClient.add({
-//   plugin: new AdvertisingIdPlugin(),
-// });
 
 const MainStack = createStackNavigator();
 const RootStack = createStackNavigator();
+
 function MainStackScreen() {
   return (
     <MainStack.Navigator
       screenOptions={{
-        headerStyle: {
-          backgroundColor: '#262e4f',
-        },
+        headerStyle: { backgroundColor: '#262e4f' },
         headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
+        headerTitleStyle: { fontWeight: 'bold' },
       }}>
-      <MainStack.Screen
-        name="Home"
-        component={Home}
-        options={{headerShown: false}}
-      />
-      <MainStack.Screen
-        name="SecondPage"
-        component={SecondPage}
-        options={{title: 'Second Page'}}
-      />
+      <MainStack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <MainStack.Screen name="SecondPage" component={SecondPage} options={{ title: 'Second Page' }} />
     </MainStack.Navigator>
   );
 }
 
-const getActiveRouteName = (
-  state: NavigationState | PartialState<NavigationState> | undefined,
-): string => {
-  if (!state || typeof state.index !== 'number') {
-    return 'Unknown';
-  }
-
+const getActiveRouteName = (state): string => {
+  if (!state || typeof state.index !== 'number') return 'Unknown';
   const route = state.routes[state.index];
-
-  if (route.state) {
-    return getActiveRouteName(route.state);
-  }
-
-  return route.name;
+  return route.state ? getActiveRouteName(route.state) : route.name;
 };
 
 const App = () => {
-  // React.useEffect(() => {
-  //   void RNBootSplash.hide();
-  // }, []);
-
+  const [segmentClient, setSegmentClient] = useState<SegmentClient | undefined>(undefined);
   const [routeName, setRouteName] = useState('Unknown');
+
+  useEffect(() => {
+    const initializeSDKs = async () => {
+      console.log('Initializing OneTrust SDK...');
+      try {
+        const responseObject = await OTPublishersNativeSDK.startSDK(
+          'cdn.cookielaw.org',
+          '018f7e67-ea32-753c-9220-2eb2c94f452b-test',
+          'en',
+          { countryCode: 'us', regionCode: 'ca' },
+          true
+        );
+
+        const typedResponse = responseObject as { responseString: string };
+        console.log('OneTrust SDK initialized. Download status:', typedResponse.responseString);
+
+        console.log('Initializing Segment client...');
+        const client = createClient({
+          writeKey: 'VRzdzieruD0trB00Tl7n8cdQDyEI8qIK',
+          trackAppLifecycleEvents: true,
+          collectDeviceId: true,
+          debug: true,
+          trackDeepLinks: true,
+          flushPolicies: [new CountFlushPolicy(5)],
+        });
+
+        const applicableCategories = ['C0004'];
+        const oneTrustProvider = new OneTrustConsentProvider(OTPublishersNativeSDK);
+        oneTrustProvider.setApplicableCategories(applicableCategories);
+
+        const oneTrustPlugin = new ConsentPlugin(oneTrustProvider);
+        client.add({ plugin: oneTrustPlugin });
+
+        oneTrustPlugin.start();
+        console.log('OneTrust plugin started.');
+
+        console.log('Segment consent settings:', client.consentSettings.get());
+        setSegmentClient(client);
+      } catch (error) {
+        console.error('OneTrust SDK initialization failed:', error);
+      }
+    };
+
+    initializeSDKs();
+  }, []);
+
+  // Show nothing while SDKs initialize
+  if (!segmentClient) {
+    console.log('Waiting for SDK initialization...');
+    return null;
+  }
 
   return (
     <AnalyticsProvider client={segmentClient}>
       <NavigationContainer
-        onStateChange={state => {
+        onStateChange={(state) => {
           const newRouteName = getActiveRouteName(state);
-
-          if (routeName !== newRouteName) {
+          if (segmentClient && routeName !== newRouteName) {
+            console.log(`Navigated to screen: ${newRouteName}`);
             void segmentClient.screen(newRouteName);
-
             setRouteName(newRouteName);
           }
         }}>
         <RootStack.Navigator
           screenOptions={{
-            headerStyle: {
-              backgroundColor: '#262e4f',
-            },
+            headerStyle: { backgroundColor: '#262e4f' },
             headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
+            headerTitleStyle: { fontWeight: 'bold' },
             presentation: 'modal',
           }}>
-          <RootStack.Screen
-            name="Main"
-            component={MainStackScreen}
-            options={{headerShown: false}}
-          />
-          <RootStack.Screen
-            name="Modal"
-            component={Modal}
-            options={{headerBackTitle: 'Go back'}}
-          />
+          <RootStack.Screen name="Main" component={MainStackScreen} options={{ headerShown: false }} />
+          <RootStack.Screen name="Modal" component={Modal} options={{ headerBackTitle: 'Go back' }} />
         </RootStack.Navigator>
       </NavigationContainer>
     </AnalyticsProvider>
@@ -188,3 +354,4 @@ const App = () => {
 };
 
 export default App;
+
--- examples/AnalyticsReactNativeExample/consent/OneTrustProvider.ts
+++ examples/AnalyticsReactNativeExample/consent/OneTrustProvider.ts
@@ -23,33 +23,53 @@ export class OneTrustConsentProvider implements CategoryConsentStatusProvider {
   getConsentStatus!: () => Promise<Record<string, boolean>>;
   private onConsentChangeCallback!: OnConsentChangeCb;
 
-  constructor(private oneTrust: OTPublishersNativeSDK) {}
+  constructor(private oneTrust: OTPublishersNativeSDK) {
+    console.log('[OneTrustConsentProvider] Initialized with OneTrust instance');
+  }
 
   onConsentChange(cb: (updConsent: Record<string, boolean>) => void): void {
-    console.log('test');
+    console.log('[onConsentChange] Callback registered');
     this.onConsentChangeCallback = cb;
   }
 
   setApplicableCategories(categories: string[]): void {
-    console.log('setApplicableCategories');
+    console.log('[setApplicableCategories] Called with categories:', categories);
+    
     const initialStatusesP = Promise.all(
-      categories.map((categoryId) =>
-        this.oneTrust
+      categories.map((categoryId) => {
+        console.log(`[getConsentStatusForCategory] Fetching for categoryId: ${categoryId}`);
+        return this.oneTrust
           .getConsentStatusForCategory(categoryId)
-          .then<[string, boolean]>((status) => [
-            categoryId,
-            status === ConsentStatus.Granted,
-          ])
-      )
-    ).then((entries) => Object.fromEntries(entries));
+          .then<[string, boolean]>((status) => {
+            const granted = status === ConsentStatus.Granted;
+            console.log(`[getConsentStatusForCategory] Status for ${categoryId}:`, status, `(granted: ${granted})`);
+            return [categoryId, granted];
+          });
+      })
+    ).then((entries) => {
+      const entriesObj = Object.fromEntries(entries);
+      console.log('[initialStatusesP] Consent entries:', entriesObj);
+      return entriesObj;
+    });
+
     let latestStatuses: Record<string, boolean> | null;
 
-    this.getConsentStatus = () =>
-      Promise.resolve(latestStatuses ?? initialStatusesP);
+    this.getConsentStatus = () => {
+      const toReturn = latestStatuses ?? initialStatusesP;
+      console.log('[getConsentStatus] Returning:', toReturn);
+      return Promise.resolve(toReturn);
+    };
+
+    console.log('[setApplicableCategories] Stopping any existing listeners');
     this.oneTrust.stopListeningForConsentChanges();
+
+    console.log('[setApplicableCategories] Setting allowed values for categories');
     this.oneTrust.setBroadcastAllowedValues(categories);
+
     categories.forEach((categoryId) => {
+      console.log(`[setApplicableCategories] Listening for consent changes for ${categoryId}`);
       this.oneTrust.listenForConsentChanges(categoryId, (_, status) => {
+        console.log(`[listenForConsentChanges] Detected change for ${categoryId}:`, status);
         initialStatusesP
           .then((initialStatuses) => {
             latestStatuses = {
@@ -57,10 +77,11 @@ export class OneTrustConsentProvider implements CategoryConsentStatusProvider {
               ...latestStatuses,
               [categoryId]: status === ConsentStatus.Granted,
             };
-
+            console.log('[Consent Change] Updated latestStatuses:', latestStatuses);
             this.onConsentChangeCallback(latestStatuses);
           })
           .catch((e) => {
+            console.error('[Consent Change Error]', e);
             throw e;
           });
       });
@@ -68,6 +89,7 @@ export class OneTrustConsentProvider implements CategoryConsentStatusProvider {
   }
 
   shutdown() {
+    console.log('[shutdown] Stopping all consent listeners');
     this.oneTrust.stopListeningForConsentChanges();
   }
 }
