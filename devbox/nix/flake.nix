8497968fd79c8ec048904e7ae408c898d87ea0ff
Andrea Bueide <abueide@twilio.com>
2026-01-22 12:33:10 -0600

    fix cleanup

--- devbox/nix/flake.nix
+++ devbox/nix/flake.nix
@@ -29,19 +29,27 @@
             };
           };
 
-          androidPkgs = pkgs.androidenv.composeAndroidPackages {
-            # Keep API 21 images for the AVD and add API 33 for React Native builds.
-            platformVersions = [ "21" "33" ];
-            buildToolsVersions = [ "30.0.3" "33.0.0" "latest" ];
-            cmdLineToolsVersion = "19.0";
-            includeEmulator = true;
-            includeSystemImages = true;
-            includeNDK = true;
-          };
+          mkSdk = platformVersions:
+            pkgs.androidenv.composeAndroidPackages {
+              platformVersions = platformVersions;
+              buildToolsVersions = [ "30.0.3" "33.0.0" "latest" ];
+              cmdLineToolsVersion = "19.0";
+              includeEmulator = true;
+              includeSystemImages = true;
+              includeNDK = true;
+              abiVersions = [ "x86_64" "arm64-v8a" ];
+              systemImageTypes = [ "google_apis" ];
+            };
+
+          sdkMin = mkSdk [ "21" ];
+          sdkLatest = mkSdk [ "33" ];
+          sdkFull = mkSdk [ "21" "33" ];
         in
         {
-          android-sdk = androidPkgs.androidsdk;
-          default = androidPkgs.androidsdk;
+          android-sdk-min = sdkMin.androidsdk;
+          android-sdk-latest = sdkLatest.androidsdk;
+          android-sdk = sdkFull.androidsdk;
+          default = sdkFull.androidsdk;
         });
     };
 }
