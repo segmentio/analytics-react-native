d39ed1c26a5f793f54e88815efa780debbc99afa
Sunita Prajapati <sunita.kuamari@daffodilsw.com>
2026-04-21 12:48:48 +0530

    refactor(config): formatted config file

--- multi-release.config.js
+++ multi-release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   branches: [
     'master',
     { name: '+([0-9])?(.{+([0-9]),x}).x', prerelease: true }, // support branches (e.g., 1.x, 1.2.x)
-    { name: 'beta', prerelease: true}, // explicit beta channel
+    { name: 'beta', prerelease: true }, // explicit beta channel
     { name: 'fix/*', prerelease: true }, // fix branches → x.x.x-fix.N
     { name: 'feat/*', prerelease: true }, // feature branches → x.x.x-feat.N
   ],
